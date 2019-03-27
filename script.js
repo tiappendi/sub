62154749fbe0c8477730f913776f3cbf86d1e6f3
Giulio Garofalo <tiappendi@gmail.com>
2019-03-27 11:10:59 +0100

    replaced show animation with jquery - missing fonts and input colors

--- script.js
+++ script.js
@@ -1,40 +1,26 @@
 function showHide (elem) {
     if(elem.selectedIndex != 0) {
-        document.getElementById('attendee_container').style.display = 'block';
+        jQuery('#attendee_container').hide("linear").show("linear").fadeIn();
         var elements = document.querySelectorAll(".wrap");
         Array.from(elements).forEach(function(element,i) { 
-            if (i >= elem.selectedIndex) {
-                element.classList.add('d-none');
-                element.classList.remove('d-block');                    
-            } else {
-                element.classList.remove('d-none');
-                element.classList.add('d-block');
-            }
+            i >= elem.selectedIndex ? jQuery(element).fadeOut("fast").addClass('d-none').removeClass('d-block') : jQuery(element).fadeIn("fast").addClass('d-block').removeClass('d-none');
         });
-        testAnim('bounceInDown');
-        validEqVisib();
+        setTimeout(function(){
+            validEqVisib();
+        },200);
     } else {
-        document.getElementById('attendee_container').style.display = 'none';
+        jQuery('#attendee_container').fadeOut();
     }
-}
-function testAnim(x) {
-    // var attendee = document.getElementById('attendee_container');
-    // document.querySelector('.collapsible').classList.toggle('collapsed');
-    // attendee.className = '';
-//     attendee.classList.add('collapsible');
-//     setTimeout(function() {
-//         attendee.className = '';
-//     }, 1000);
 }
 
 function validEqVisib() {
     var visible = document.querySelectorAll('.wrap.d-block');
     var valid = document.querySelectorAll('.d-block .valid');
     if (valid.length == visible.length){
-        document.querySelector('#step1_result').classList.add('d-block');
+        jQuery('#step1_result').show();
         document.getElementById('step_2').disabled = false;
     }else {
-        document.querySelector('#step1_result').classList.remove('d-block');
+        jQuery('#step1_result').hide();
         document.getElementById('step_2').disabled = true;
     }
 }
@@ -47,8 +33,6 @@ function companyName() {
 (function(){
     "use strict";
     document.addEventListener("DOMContentLoaded", function(event) {
-
-        // document.getElementById('num_attendees').addEventListener('onchange', showHide(this));
 
         // field_1
         var glInput = document.querySelectorAll('#attendee_container input');
@@ -111,28 +95,8 @@ function companyName() {
         });
 
         document.getElementById('company_name').addEventListener('keyup', function () {
-            var that = this;
-            that.value != '' && rad1_checked && rad2_checked ? document.getElementById('step_3').disabled = false : document.getElementById('step_3').disabled = true;
+            companyName();
         });
 
-        function showHide (elem) {
-            if(elem.selectedIndex != 0) {
-                document.getElementById('attendee_container').style.display = 'block';
-                var elements = document.querySelectorAll(".wrap");
-                Array.from(elements).forEach(function(element,i) { 
-                    if (i >= elem.selectedIndex) {
-                        element.classList.add('d-none');
-                        element.classList.remove('d-block');                    
-                    } else {
-                        element.classList.remove('d-none');
-                        element.classList.add('d-block');
-                    }
-                });
-                testAnim('bounceInDown');
-                validEqVisib();
-            } else {
-                document.getElementById('attendee_container').style.display = 'none';
-            }
-        }     
     });
 })();
